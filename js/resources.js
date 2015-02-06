5451ec5933ce248ea777d30949ca8476857c8772
cesar arroyo <cesararroyo1234@gmail.com>
2015-02-05 20:11:13 -0800

    testing if source tree works

--- js/resources.js
+++ js/resources.js
@@ -4,10 +4,12 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"} ,
-        {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"} ,
-        {name: "player", type:"image", src: "data/img/orcSpear.png"} ,//this calls the player tile and loads it
-
+	 // loads our background tiles
+	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	 {name: "player", type:"image", src: "data/img/orcSpear.png"}, 
+	 {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
+	 {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -18,7 +20,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        {name: "test", type: "tmx", src: "data/map/test.tmx"},//this calls the map from the map folder 
+ 	 // loads level01 
+ 	 {name: "level1", type: "tmx", src: "data/map/level1.tmx"},
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
--- js/screens/play.js
+++ js/screens/play.js
@@ -1,29 +1,44 @@
 game.PlayScreen = me.ScreenObject.extend({
-    /**
-     *  action to perform on state change
-     */
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
 
-    onResetEvent: function() {
-        // reset the score
-        game.data.score = 0;
+		// loads level 1 in play.js so that it will show up on your screen
+		me.levelDirector.loadLevel("level1");
+
+		//loads the plyer so that it will show up when you run it
+		var player = me.pool.pull("player", 0, 420, {});
+		// adds player to the world
+		me.game.world.addChild(player, 5);
+
+		var gamemanager = me.pool.pull("GameManager", 0, 0, {});
+		me.game.world.addChild(gamemanager, 0);
+
+		me.input.bindKey(me.input.KEY.RIGHT, "right");
+
+		me.input.bindKey(me.input.KEY.LEFT, "left");
+
+		me.input.bindKey(me.input.KEY.SPACE, "jump");
+
+		me.input.bindKey(me.input.KEY.A, "attack");
+		
 
 
-        me.levelDirector.loadLevel("test");
-        //this loads the level "test"
-        var player = me.pool.pull("player", 0, 420, {});
-        me.game.world.addChild(player,  5);
-        
-        me.input.bindKey(me.input.KEY.RIGHT, "right");//looks for the rigt key to be pressed
-        me.input.bindKey(me.input.KEY.LEFT, "left");
-        // add our HUD to the game world
-        this.HUD = new game.HUD.Container();
-        me.game.world.addChild(this.HUD);
-    },
-    /**
-     *  action to perform when leaving this screen (state change)
-     */
-    onDestroyEvent: function() {
-        // remove the HUD from the game world
-        me.game.world.removeChild(this.HUD);
-    }
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
 });
